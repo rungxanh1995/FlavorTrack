b9b449d77abf77a435b952fe7309317187780dba
Joe Pham <rungxanh2901@gmail.com>
2023-01-14 17:11:08 -0500

    Initial Commit

---
+++ FlavorTrack/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlavorTrack
+//
+//  Created by Joe Pham on 2023-01-14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
